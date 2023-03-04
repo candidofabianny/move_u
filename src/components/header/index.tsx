cd94ef59e4aebd5d5493b4f24d5fe9cdc99bef09
candidofabianny <candidofabianny@gmail.com>
2023-03-04 19:06:18 -0300

    header and scss components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,1 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "/public/images/Vector.svg"
+import Title from "/public/images/logo_title.svg"
+import Link from "next/link";
+import Styles from "./header.module.scss";
+
+const Header = () => {
+    return <div className={Styles.container}>
+        <div className={Styles.logo}>
+            <Image src={Logo} alt={""} />
+
+        </div>
+        <div className={Styles.menu}>
+                <Link href="/">Home</Link>
+                <Link href="/">About</Link>
+                <Link href="/">Listing</Link>
+                <Link href="/">Contact</Link>
+        </div>
+        <div className={Styles.buttonAction}>
+                <button className={Styles.buttonshare}>Share it</button>
+        </div>
+    </div>
+
+}
+
+export default Header; 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/pages/Home.module.scss'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p>Hello</p>
+      <Header />
       </div>
   )
 }
