2a964827a2192121980f0d75f7d5be35bca92861
candidofabianny <candidofabianny@gmail.com>
2023-03-05 10:03:51 -0300

    button and first page

---
+++ src/components/first_page/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Styles from "./firstp.module.css"
+import {linkRel} from 'link-rel'
+
+const First_page = () => {
+    return <div className={Styles.container}>
+        <div className="background">
+            <div/>
+
+        </div>
+    </div>
+
+}
+
+export default First_page; 
--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -4,7 +4,9 @@ import Styles from "./welcome.module.css";
 const Welcome = () => {
     return <div className={Styles.container}>
         <div className={Styles.text}></div>
-        <h1 className={Styles.h1}>MoveU My travel bucket list from movies</h1>
+        <h1 className={Styles.h1}>MoveU</h1>
+        <h2 className={Styles.h2}>My travel bucket</h2>
+        <h2 className={Styles.h2}>list from movies</h2>
         <p className={Styles.p}>Movies scenarios that can inspire you to travel</p>
         <button className={Styles.buttonshare}>Contact me</button>
         <div className={Styles.box}></div>
