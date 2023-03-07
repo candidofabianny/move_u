b3d34de8a5953a548bc697ef61374f3ac4e64a57
candidofabianny <candidofabianny@gmail.com>
2023-03-06 22:57:52 -0300

    nome do commit

--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import Styles from "./welcome.module.css";
 import Center from "/public/images/Center.svg"
 import Maps from "/public/images/maps_icon.svg"
-import Elipse from "/public/images/Elipse_maior.svg"
 
 const Welcome = () => {
     return <div className={Styles.container}>
@@ -15,8 +14,8 @@ const Welcome = () => {
         <Image src={Maps} alt={"Maps icon"} />
         </div>
         <h1 className={Styles.h1}>MoveU</h1>
-        <h2 className={Styles.h2}>Travel bucket</h2>
-        <h2 className={Styles.h2}>list from movies</h2>
+        <h1 className={Styles.h2}>Travel bucket</h1>
+        <h1 className={Styles.h2}>list from movies</h1>
         <p className={Styles.p}>Movies scenarios that can inspire you to travel</p>
         <button className={Styles.buttonshare}>Contact me</button>
         
